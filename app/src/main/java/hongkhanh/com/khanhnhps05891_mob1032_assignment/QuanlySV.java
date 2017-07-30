e9dedc6de03f83a1aa0b2df0110aebe414acb6aa
khanh10101998 <khanh10101998@gmail.com>
2017-07-30 16:18:52 +0700

    Initial commit

---
+++ app/src/main/java/hongkhanh/com/khanhnhps05891_mob1032_assignment/QuanlySV.java
@@ -0,0 +1,13 @@
+package hongkhanh.com.khanhnhps05891_mob1032_assignment;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class QuanlySV extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quanly_sv);
+    }
+}
